e8a9a680d7d786a437e4efe304bb5254df84ee0c
Nathaniel <nthnlrichards@gmail.com>
2021-10-23 11:29:44 -0400

    Adding wordlposition class

--- Source/BuildingEscape/WorldPosition.h
+++ Source/BuildingEscape/WorldPosition.h
@@ -6,23 +6,21 @@
 #include "Components/ActorComponent.h"
 #include "WorldPosition.generated.h"
 
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BUILDINGESCAPE_API UWorldPosition : public UActorComponent {
+  GENERATED_BODY()
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UWorldPosition : public UActorComponent
-{
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UWorldPosition();
+public:
+  // Sets default values for this component's properties
+  UWorldPosition();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+  // Called when the game starts
+  virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+public:
+  // Called every frame
+  virtual void
+  TickComponent(float DeltaTime, ELevelTick TickType,
+                FActorComponentTickFunction *ThisTickFunction) override;
 };
